368646a0a626d0d1bcf7efebd660ab8afa5b6171
Dann Cuapiaco <cuapiaco.dann@yahoo.com>
2022-09-22 10:32:28 +0800

    edit linkedin url

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ Senior Software Engineer, providing technical solutions through high-quality ser
 """
 EMAIL = "cuapiaco.dann@yahoo.com"
 SOCIAL_MEDIA = {
-    "LinkedIn": "https://www.linkedin.com/in/dann-guiller-cuapiaco-613012133/",
+    "LinkedIn": "www.linkedin.com/in/dann-guiller-cuapiaco",
     "GitHub": "https://github.com/daerco22",
 	"Facebook": "https://www.facebook.com/danncuapiaco"
 }
